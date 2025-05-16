479274cdd20d50a8eb2773a226dc2f8136c12aa5
sevenjune <yahiawissal76@gmail.com>
2025-05-16 03:29:27 +0100

    Update likelihoodratios.py

--- likelihoodratios.py
+++ likelihoodratios.py
@@ -8,44 +8,5 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import class_likelihood_ratios
-estimator = LogisticRegression().fit(X_train, y_train)
-y_pred = estimator.predict(X_test)
-pos_LR, neg_LR = class_likelihood_ratios(y_test, y_pred)
-print(f"LR+: {pos_LR:.3f}")
-
-#Cross-validation of likelihood ratios
-
-import pandas as pd
 
 
-def scoring(estimator, X, y):
-    y_pred = estimator.predict(X)
-    pos_lr, neg_lr = class_likelihood_ratios(y, y_pred, raise_warning=False)
-    return {"positive_likelihood_ratio": pos_lr, "negative_likelihood_ratio": neg_lr}
-
-
-def extract_score(cv_results):
-    lr = pd.DataFrame(
-        {
-            "positive": cv_results["test_positive_likelihood_ratio"],
-            "negative": cv_results["test_negative_likelihood_ratio"],
-        }
-    )
-    return lr.aggregate(["mean", "std"])
-
-from sklearn.model_selection import cross_validate
-estimator = LogisticRegression()
-extract_score(cross_validate(estimator, X, y, scoring=scoring, cv=10))
-#______________
-from sklearn.dummy import DummyClassifier
-estimator = DummyClassifier(strategy="stratified", random_state=1234)
-extract_score(cross_validate(estimator, X, y, scoring=scoring, cv=10))
-#______________
-estimator = DummyClassifier(strategy="most_frequent")
-extract_score(cross_validate(estimator, X, y, scoring=scoring, cv=10))
-#______________
-estimator = LogisticRegression()
-X, y = make_classification(n_samples=300, weights=[0.9, 0.1], random_state=0)
-extract_score(cross_validate(estimator, X, y, scoring=scoring, cv=10))
